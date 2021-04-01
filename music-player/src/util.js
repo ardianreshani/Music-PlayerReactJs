b26cc79868ee5834125cf34a7af6cbc25c79a783
aRdiiaNaRdiiaN04 <youngcreative12@gmail.com>
2021-04-01 21:20:22 +0200

    songUpdate

--- music-player/src/util.js
+++ music-player/src/util.js
@@ -2,20 +2,20 @@ import { v4 as uuidv4 } from "uuid";
 function chillHop() {
   return [
     {
-      name: "Beaver Creek",
+      name: "Green House",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+        "https://chillhop.com/wp-content/uploads/2020/07/1ca6410720af4607262d8c64afac2b83670d5a3b-1024x1024.jpg",
+      artist: "Mo Anando",
+      audio: "../public/music/test.m3",
       color: ["#205950", "#2ab3bf"],
       id: uuidv4(),
       active: true,
     },
     {
-      name: "Daylight",
+      name: "Toft",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
+        "https://chillhop.com/wp-content/uploads/2020/07/858b533ba20ff95bf5b401089b195d1a8cb43870-1024x1024.jpg",
+      artist: "Felty, Ezzy",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
       color: ["#EF8EA9", "#ab417f"],
       id: uuidv4(),
